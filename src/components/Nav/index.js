2c79c2609c81ba8a35e2ff3202035f6d40674e8f
jyoung0613 <jwyoung0613@gmail.com>
2021-11-30 11:33:16 -0700

    resloved issues page up with pictures now

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,25 +17,28 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            📸
-          </span>
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">About me</a>
+            <a data-testid="about" href="#about">
+            About me
+            </a>
           </li>
           <li className="mx-2">
-            <span>Contact</span>
+            <span>
+            Contact
+            </span>
           </li>
           {categories.map((category) => (
             <li className={`mx-1 ${
               currentCategory.name === category.name && 'navActive'
             }`} key={category.name}>
-              <span onClick={() => { setCurrentCategory(category)
+              <span 
+              onClick={() => { 
+                setCurrentCategory(category)
               }}
               >
                 {capitalizeFirstLetter(category.name)}
