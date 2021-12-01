e04ad38b0d86436bbf9ca0763bf948f0fd75a153
jyoung0613 <jwyoung0613@gmail.com>
2021-12-01 08:44:22 -0700

    now have the contact form fully functional and returns the data entered

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,41 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({ name: "", email: "", message: "" });
 
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
     }
+  };
 
-    const { name, email, message } = formState;
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
+  function handleChange(e) {
     
-    return (
-        <section>
-            <h1>Contact me</h1>
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input onChange={handleChange} type="text" name="name" defaultValue={name} />
-                </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input onChange={handleChange} type="email" name="email" defaultValue={email} />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea onChange={handleChange} name="message" rows="5" defaultValue={message} />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    );
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+    console.log('errorMessage', errorMessage);
+  }
+
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            defaultValue={name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            defaultValue={email}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            onChange={handleChange}
+            name="message"
+            rows="5"
+            defaultValue={message}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
